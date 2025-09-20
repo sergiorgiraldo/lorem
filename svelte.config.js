a5607990c8ab1b325f32eec5b570a0b128d250db
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2025-09-20 20:15:31 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+//import { vitePreprocess } from '@sveltejs/kit/vite';
+import { viteprocess } from @sveltejs/vite-plugin-svelte; 
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
